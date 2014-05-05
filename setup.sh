91036566a6ad61347e9c6edac7226748a2779bbf
Quinten Palmer <quintenpalmer@gmail.com>
2014-05-05 18:10:44 -0400

    Formalize setup.sh

--- setup.sh
+++ setup.sh
@@ -4,13 +4,48 @@ ME=$(basename $0)
 DATABASE_DIR="$(dirname $(realpath ${ME}))/database"
 TEMPLATE_DIR="${DATABASE_DIR}/templates"
 
-rm -r ${TEMPLATE_DIR}
-mkdir -p ${TEMPLATE_DIR}
+function usage() {
+	echo "either supply no arguments \"deps\" or \"sync\""
+}
 
-pushd ../game_engine
-GOPATH=$(pwd) go run templater.go -filepath=${TEMPLATE_DIR}
-popd
+function setup_database() {
+	rm database/db.sqlite3
+	./database/sql_scripts/create_db.py
+	sync_database
+}
 
-rm database/db.sqlite3
-./database/sql_scripts/create_db.py
-./database/sql_scripts/seed_db.py
+function sync_database() {
+	./database/sql_scripts/seed_db.py game_engine
+	generate_templates
+	./database/sql_scripts/seed_db.py template
+}
+
+function generate_templates() {
+	rm -r ${TEMPLATE_DIR}
+	mkdir -p ${TEMPLATE_DIR}
+
+	pushd ../game_engine
+	GOPATH=$(pwd) go run templater.go -filepath=${TEMPLATE_DIR}
+	popd
+}
+
+function install_deps() {
+	sudo pip install -r requirements.txt
+}
+
+function main() {
+	echo $#
+	if [ $# -lt 1 ]; then
+			setup_database
+	else
+		if [ $1 == 'deps' ]; then
+			install_deps
+		elif [ $1 == 'sync' ]; then
+			sync_database
+		else
+			usage
+		fi
+	fi
+}
+
+main $@
