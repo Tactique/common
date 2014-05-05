4e2f608f57a4902366f5d780eec285bb6f24ad91
Quinten Palmer <quintenpalmer@gmail.com>
2014-05-05 16:29:32 -0400

    Generate template responses instead of tracking in git

--- setup.sh
+++ setup.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+ME=$(basename $0)
+DATABASE_DIR="$(dirname $(realpath ${ME}))/database"
+TEMPLATE_DIR="${DATABASE_DIR}/templates"
+
+rm -r ${TEMPLATE_DIR}
+mkdir -p ${TEMPLATE_DIR}
+
+pushd ../game_engine
+GOPATH=$(pwd) go run templater.go -filepath=${TEMPLATE_DIR}
+popd
+
 rm database/db.sqlite3
 ./database/sql_scripts/create_db.py
 ./database/sql_scripts/seed_db.py
