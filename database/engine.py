b4224797f6d416f95e54d1c0cfc68eced4f529ee
Quinten Palmer <quintenpalmer@gmail.com>
2014-05-04 21:58:12 -0400

    Later self assignment and remove tabs in engine

--- database/engine.py
+++ database/engine.py
@@ -5,19 +5,19 @@ from sqlalchemy.orm import sessionmaker
 
 
 class Engine:
-	def __init__(self):
-		db_file = 'sqlite:////%s' % os.path.join(os.environ['ROOTIQUE'], 'common', 'database', 'db.sqlite3')
-		print(db_file)
-		self.engine = sqla.create_engine(db_file, echo=False)
+    def __init__(self):
+        db_file = 'sqlite:////%s' % os.path.join(os.environ['ROOTIQUE'], 'common', 'database', 'db.sqlite3')
+        self.engine = sqla.create_engine(db_file, echo=False)
+        my_sessionmaker = sessionmaker()
+        my_sessionmaker.configure(bind=self.engine)
 
-		self.my_sessionmaker = sessionmaker()
-		self.my_sessionmaker.configure(bind=self.engine)
+        self.my_sessionmaker = my_sessionmaker
 
-	def get_session(self):
-		return self.my_sessionmaker()
+    def get_session(self):
+        return self.my_sessionmaker()
 
-	def get_engine(self):
-		return self.engine
+    def get_engine(self):
+        return self.engine
 
 
 engine = Engine()
